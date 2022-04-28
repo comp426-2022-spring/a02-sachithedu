af076f021842af49c1923cf3d3605289af8831a2
Sachith <sachith.iyengar@gmail.com>
2022-04-28 11:23:37 -0400

    flips change

--- flips.js
+++ flips.js
@@ -1,9 +1,24 @@
-import {coinFlips, countFlips} from "./modules/coin.mjs";
-import minimist from 'minimist'; 
 
-const args = minimist(process.argv.slice(2)); 
-const number = args.number || 1; 
+import minimist from 'minimist';
+import {coinFlips, countFlips} from './modules/coin.mjs';
 
-const flips = coinFlips(number);
-console.log(flips);
-console.log(countFlips(flips)); 
+console.log("In flips.js")
+console.log(process.argv)
+const args = minimist(process.argv.slice(2))
+
+if (args.number == undefined) {
+    args.number = 1;
+}
+
+let array = coinFlips(args.number);
+
+if (args.number == 1) {
+    if (countFlips(array).heads != 0) {
+        console.log("['heads'] {heads: " + countFlips(array).heads + "}")
+    } else {
+        console.log("['tails'] {tails: " + countFlips(array).tails + "}")
+    }
+} else {
+    console.log(array);
+    console.log(countFlips(array));
+}
