edf17e5fa52ef8d84ece6ac8dba578c1d99c2020
Sachith <sachith.iyengar@gmail.com>
2022-04-28 11:04:38 -0400

    flips edits

---
+++ modules/flips.js
@@ -0,0 +1,9 @@
+import {coinFlips, countFlips} from "./modules/coin.mjs";
+import minimist from 'minimist'; 
+
+const args = minimist(process.argv.slice(2)); 
+const number = args.number || 1; 
+
+const flips = coinFlips(number);
+console.log(flips);
+console.log(countFlips(flips)); 
